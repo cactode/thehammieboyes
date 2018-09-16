9ffd106f4901d3522ff7811498fa203219325af2
Chetan Sharma <ban@mit.edu>
2018-09-15 21:06:45 -0400

    Combined code still need to add the consumption and the conversion to crane commands

--- command_parser.py
+++ command_parser.py
@@ -1,81 +1,84 @@
-directions = ['u','d','l','r']
+directions = ['up','down','left','right', 'Up','Down','Left','Right',]
 commands = ["dip", "move"]
 commandList = []
 
 def parseyBoi(stringyBoi):
-	splitBoi = stringyBoi.splitlines()
-	for boi in splitBoi:
-		words = boi.split(" ")
-		for word in words:
-			separate = word.split(":")
-			if (separate[0] in commands):
-				parseCommand(separate)
+	# for e in stringyBoi :
+		splitBoi = stringyBoi.splitlines()
+		for boi in splitBoi:
+
+			words = boi.split(" ")
+			for word in words:
+				parseCommand(word)
+				# separate = word.split("_")
+				# if (separate[0] in commands):
+				# 	parseCommand(separate)
 
 def parseCommand(command):
 	global commandList
-	if len(command) == 1 and command[0] == "dip":
+	if command == "dip" or command in directions:
 		if len(commandList) == 10:
 			commandList = commandList[:1]
-			commandList.append(("dip", 0))
+			commandList.append(command)
 		else:
-			commandList.append(("dip",0))
-	elif command[0] == "move" and len(command) == 2:
-		(direction, distance) = parseDistance(command[1])
-		if direction != "bad":
-			if len(commandList) == 10:
-				commandList = commandList[:1]
-				commandList.append((direction, distance))
-			else:
-				commandList.append((direction, distance))
+			commandList.append(command)
+	# elif command in commands:
+	# 	# (direction, distance) = parseDistance(command[1])
+	# 	# if direction != "bad":
+	# 		if len(commandList) == 10:
+	# 			commandList = commandList[:1]
+	# 			commandList.append((direction, distance))
+	# 		else:
+	# 			commandList.append((direction, distance))
 	else:
-		print "invalid command attempted: ", command[0]
+		print ("invalid command attempted: ", command)
 
 
-def parseDistance(movement):
-	if len(movement) < 2:
-		return ("bad", 0)
-	direction = "bad"
-	firstLetter = movement[0]
-	rest = movement[1:]
-	if firstLetter == 'a':
-		angle = parseAngle(rest)
-		if angle[0] != "bad":
-			return ('a', angle)
-		else:
-			return angle
-	elif firstLetter in directions:
-		direction = firstLetter
-		try:
-			float(rest)
-			return (direction, float(rest))
-		except ValueError:
-			return ("bad", 0)
-	return ("bad", 0)
+# def parseDistance(movement):
+# 	if len(movement) < 2:
+# 		return ("bad", 0)
+# 	direction = "bad"
+# 	firstLetter = movement[0]
+# 	rest = movement[1:]
+# 	if firstLetter == 'a':
+# 		angle = parseAngle(rest)
+# 		if angle[0] != "bad":
+# 			return ('a', angle)
+# 		else:
+# 			return angle
+# 	elif firstLetter in directions:
+# 		direction = firstLetter
+# 		try:
+# 			float(rest)
+# 			return (direction, float(rest))
+# 		except ValueError:
+# 			return ("bad", 0)
+# 	return ("bad", 0)
 
-def parseAngle(angle):
-	if "_" in angle:
-		values = angle.split("_")
-		try:
-			float(values[0])
-			float(values[1])
-			return (float(values[0]), float(values[1]))
-		except ValueError:
-			return ("bad", "bad")
-	else:
-		return ("bad", "bad")
+# def parseAngle(angle):
+# 	if "_" in angle:
+# 		values = angle.split("_")
+# 		try:
+# 			float(values[0])
+# 			float(values[1])
+# 			return (float(values[0]), float(values[1]))
+# 		except ValueError:
+# 			return ("bad", "bad")
+# 	else:
+# 		return ("bad", "bad")
 
 def executeDip():
-	print "dip executed"
-	#some sort of code to execute the dip. Have to know how this works with the arduino/raspberry pi
+	print ("dip executed")
+	#some sort of  code to execute the dip. Have to know how this works with the arduino/raspberry pi
 	return
 
 def executeLinearMove(direction, distance):
-	print "move executed in direction: ", direction, "with distance: ", distance
+	print ("move executed in direction: ", direction, "with distance: ", distance)
 	#Same as above
 	return
 
 def executeAngularMove(direction, distance):
-	print "move executed in direction: ", direction, "with distance: ", distance
+	print ("move executed in direction: ", direction, "with distance: ", distance)
 	#Same as above
 	return
 
@@ -85,9 +88,6 @@ hfdhsdfjhsdfjh l
 dip move
 move:a8_9"""
 
-parseyBoi(testText)
-print commandList
-
 def mainLoop(twitchList):
 	# Checks the logs from the twitch chat, using up to ten of them
 	maxRange = len(twitchList)
@@ -95,6 +95,7 @@ def mainLoop(twitchList):
 		maxRange = 10
 	for x in range (0,maxRange):
 		parseyBoi(twitchList[x])
+	print(commandList)
 
 	#execute the first thing on the command list
 
--- test.py
+++ test.py
@@ -1,17 +1,46 @@
 import socket
+import time
+import sys
+from command_parser import mainLoop
+from skycrane.config import NICK, PASS, PORT, HOST  
 
-TWITCH_USERNAME = "thehammieboyes"
-TWITCH_OATH = "oauth:pvvnv26e9y88bro7yyjfbvp6orzdj3"
+interval = 5
+words = []
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-irc = socket.socket()
-print("Connecting\n")
-irc.connect(("irc.chat.twitch.tv", 6667))
+try:
+    s.connect((HOST, PORT))
+except :
+    sys.exit("Did not connect to host\n")
 
-print("Joining\n")
-password = "PASS " + TWITCH_OATH + "\r\n"
-irc.send(password.encode())
-username = "NICK " + TWITCH_USERNAME + "\r\n"
-irc.send(username.encode())
-irc.send("JOIN ##thehammieboyes\r\n".encode())
-irc.send("text: fuck you".encode())
+# Connecting to chat via info
+s.send(bytes("PASS " + PASS + "\r\n", "UTF-8"))
+s.send(bytes("NICK " + NICK + "\r\n", "UTF-8"))
+s.send(bytes("JOIN #" + NICK + " \r\n", "UTF-8"))
+
+print("Connected and starting")
+
+# Parsing information twitch sends back to us
+while True:
+    line = str(s.recv(1024))
+    if "End of /NAMES list" in line:
+        break
+
+# Collecting words in twitch chat in five second intervals
+myTime = time.monotonic()
+while True:   
+    for line in str(s.recv(1024)).split('\\r\\n') :           
+        parts = line.split(':')
+
+        if len(parts) < 3:
+            continue
+
+        words.append(parts[2])
+ 
+    if time.monotonic() > myTime + interval:
+        # print(words)
+        #call brenner function
+        mainLoop(words)
+        myTime = time.monotonic()
+        words = []
 
